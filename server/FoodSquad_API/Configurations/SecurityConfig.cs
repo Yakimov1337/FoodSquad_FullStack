fa24f1240c19f396ac503df9a35c0e350ef903cb
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-11-28 14:56:50 +0200

    Create SecurityConfig.cs

---
+++ server/FoodSquad_API/Configurations/SecurityConfig.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace FoodSquad_API.Configuration
+{
+    public static class SecurityConfig
+    {
+        public static IServiceCollection AddCustomAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            // JWT Authentication
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = configuration["Jwt:Issuer"],
+                        ValidAudience = configuration["Jwt:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]))
+                    };
+                });
+
+            return services;
+        }
+
+        public static IServiceCollection AddCustomAuthorization(this IServiceCollection services)
+        {
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("AdminPolicy", policy =>
+                    policy.RequireRole("ADMIN"));
+
+                options.AddPolicy("ModeratorPolicy", policy =>
+                    policy.RequireRole("MODERATOR"));
+
+                options.AddPolicy("UserPolicy", policy =>
+                    policy.RequireRole("NORMAL"));
+            });
+
+            return services;
+        }
+    }
+}
