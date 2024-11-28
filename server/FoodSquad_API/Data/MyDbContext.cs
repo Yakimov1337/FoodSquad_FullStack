a8da385f98301d17a35e27db8b8825e9d28da24a
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-11-28 14:57:48 +0200

    db context

---
+++ server/FoodSquad_API/Data/MyDbContext.cs
@@ -0,0 +1,56 @@
+﻿using FoodSquad_API.Models.Entity;
+using FoodSquad_API.Models.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace FoodSquad_API.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<MenuItem> MenuItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Token> Tokens { get; set; }
+        public DbSet<OrderMenuItem> OrderMenuItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Users -> Reviews
+            modelBuilder.Entity<Review>()
+                .HasOne(r => r.User)
+                .WithMany(u => u.Reviews)
+                .HasForeignKey(r => r.UserId)
+                .OnDelete(DeleteBehavior.Restrict); // Disable cascading delete
+
+            // Users -> MenuItems
+            modelBuilder.Entity<MenuItem>()
+                .HasOne(mi => mi.User)
+                .WithMany(u => u.MenuItems)
+                .HasForeignKey(mi => mi.UserId)
+                .OnDelete(DeleteBehavior.Restrict); // Disable cascading delete
+
+            // Users -> Orders
+            modelBuilder.Entity<Order>()
+                .HasOne(o => o.User)
+                .WithMany(u => u.Orders)
+                .HasForeignKey(o => o.UserId)
+                .OnDelete(DeleteBehavior.Restrict); // Disable cascading delete
+
+            // Order -> MenuItems many-to-many relationship
+            modelBuilder.Entity<OrderMenuItem>()
+                .HasOne(om => om.Order)
+                .WithMany(o => o.MenuItemsWithQuantity)
+                .HasForeignKey(om => om.OrderId)
+                .OnDelete(DeleteBehavior.Cascade); // Cascade deletes here
+
+            modelBuilder.Entity<OrderMenuItem>()
+                .HasOne(om => om.MenuItem)
+                .WithMany()
+                .HasForeignKey(om => om.MenuItemId)
+                .OnDelete(DeleteBehavior.Cascade); // Cascade deletes here
+        }
+    }
+}
---
+++ server/FoodSquad_API/Data/MyDbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace FoodSquad_API.Data
+{
+    public class MyDbContextFactory : IDesignTimeDbContextFactory<MyDbContext>
+    {
+        public MyDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<MyDbContext>();
+
+            // Load the configuration
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new MyDbContext(optionsBuilder.Options);
+        }
+    }
+}
