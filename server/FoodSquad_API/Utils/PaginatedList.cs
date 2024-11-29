a8f72d4e0e215086f69fd8dbf042cbfae2c83c57
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-11-29 03:20:56 +0200

    Create PaginatedList.cs

---
+++ server/FoodSquad_API/Utils/PaginatedList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FoodSquad_API.Utilities
+{
+    public class PaginatedList<T>
+    {
+        public List<T> Items { get; private set; }
+        public int TotalCount { get; private set; }
+        public int PageSize { get; private set; }
+        public int CurrentPage { get; private set; }
+        public int TotalPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
+
+        public bool HasPreviousPage => CurrentPage > 1;
+        public bool HasNextPage => CurrentPage < TotalPages;
+
+        public PaginatedList(List<T> items, int totalCount, int currentPage, int pageSize)
+        {
+            Items = items;
+            TotalCount = totalCount;
+            CurrentPage = currentPage;
+            PageSize = pageSize;
+        }
+
+        public static PaginatedList<T> Create(IEnumerable<T> source, int page, int pageSize)
+        {
+            var totalCount = source.Count();
+            var items = source.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+            return new PaginatedList<T>(items, totalCount, page, pageSize);
+        }
+    }
+}
