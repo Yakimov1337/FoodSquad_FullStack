6a24ecf1e9a30847e340c5ec215f55dffa06b387
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-11-30 19:10:49 +0200

    Create SwaggerDefaultValues.cs

---
+++ server/FoodSquad_API/Utils/SwaggerDefaultValues.cs
@@ -0,0 +1,25 @@
+﻿using FoodSquad_API.Models.DTO;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Linq;
+
+public class SwaggerDefaultValues : IOperationFilter
+{
+    public void Apply(OpenApiOperation operation, OperationFilterContext context)
+    {
+        if (operation.RequestBody?.Content?.ContainsKey("application/json") ?? false)
+        {
+            var jsonContent = operation.RequestBody.Content["application/json"];
+            var schema = jsonContent.Schema;
+
+            if (context.ApiDescription.ParameterDescriptions.Any(p => p.ModelMetadata?.ModelType == typeof(UserLoginDTO)))
+            {
+                jsonContent.Example = new Microsoft.OpenApi.Any.OpenApiObject
+                {
+                    ["email"] = new Microsoft.OpenApi.Any.OpenApiString("admin@example.com"),
+                    ["password"] = new Microsoft.OpenApi.Any.OpenApiString("123123")
+                };
+            }
+        }
+    }
+}
