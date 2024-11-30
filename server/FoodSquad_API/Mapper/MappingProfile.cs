e650333db7dc5639eda469a9af18df3cf313d5c0
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-11-30 19:11:34 +0200

    Create MappingProfile.cs

---
+++ server/FoodSquad_API/Mapper/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using FoodSquad_API.Models.DTO;
+using FoodSquad_API.Models.Entity;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        // Order and OrderDTO
+        CreateMap<Order, OrderDTO>()
+            .ForMember(dest => dest.UserEmail, opt => opt.MapFrom(src => src.User.Email))
+            .ForMember(dest => dest.MenuItemQuantities, opt => opt.MapFrom(src =>
+                src.MenuItemsWithQuantity.ToDictionary(m => m.MenuItem.Id, m => m.Quantity)))
+            .ReverseMap();
+    }
+}
