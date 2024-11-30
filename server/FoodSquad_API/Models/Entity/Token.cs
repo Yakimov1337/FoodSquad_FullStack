adf2936074d2c4266321fe33b397766b6513fe99
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-11-30 19:12:12 +0200

    Update Token.cs

--- server/FoodSquad_API/Models/Entity/Token.cs
+++ server/FoodSquad_API/Models/Entity/Token.cs
@@ -1,31 +1,28 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
+﻿using FoodSquad_API.Models.Entity;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
-namespace FoodSquad_API.Models.Entity
+public class Token
 {
-    public class Token
-    {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public long Id { get; set; }
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public long Id { get; set; }
 
-        [Required]
-        [Column(TypeName = "TEXT")]
-        public string AccessToken { get; set; }
+    [Required]
+    [Column(TypeName = "NVARCHAR(MAX)")] // Use NVARCHAR(MAX) instead of TEXT
+    public string AccessToken { get; set; }
 
-        [Required]
-        [Column(TypeName = "TEXT")]
-        public string RefreshToken { get; set; }
+    [Required]
+    [Column(TypeName = "NVARCHAR(MAX)")] // Use NVARCHAR(MAX) instead of TEXT
+    public string RefreshToken { get; set; }
 
-        [Required]
-        public DateTime AccessTokenExpiryDate { get; set; }
+    [Required]
+    public DateTime AccessTokenExpiryDate { get; set; }
 
-        [Required]
-        public DateTime RefreshTokenExpiryDate { get; set; }
+    [Required]
+    public DateTime RefreshTokenExpiryDate { get; set; }
 
-        [ForeignKey("User")]
-        public Guid UserId { get; set; }
-        public User User { get; set; }
-    }
+    [ForeignKey("User")]
+    public Guid UserId { get; set; }
+    public User User { get; set; }
 }
