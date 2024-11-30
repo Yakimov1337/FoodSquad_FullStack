8cbbbc3e7e032b64e935abd78da32f6f76bd6f50
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-11-30 19:12:00 +0200

    Create GlobalExceptionFilter.cs

---
+++ server/FoodSquad_API/Middleware/GlobalExceptionFilter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+
+public class GlobalExceptionFilter : IExceptionFilter
+{
+    public void OnException(ExceptionContext context)
+    {
+        var errorResponse = new
+        {
+            error = "An error occurred",
+            message = context.Exception.Message
+        };
+
+        context.Result = new JsonResult(errorResponse)
+        {
+            StatusCode = 500
+        };
+        context.ExceptionHandled = true;
+    }
+}
