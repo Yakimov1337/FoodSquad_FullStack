21ba5a0466d61c2ab0e565f90b2c9511899a4f04
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-12-02 03:06:15 +0200

    delete dtos

--- server/FoodSquad_API/Models/DTO/LogoutReqestDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FoodSquad_API.Models.DTO
-{
-    public class LogoutRequestDTO
-    {
-        public string AccessToken { get; set; }
-        public string RefreshToken { get; set; }
-    }
-}
