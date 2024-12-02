ac9955d4d6ea0c5a23e3f153b404b986a32e2e0d
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-12-02 03:06:49 +0200

    impl: user DTO's

---
+++ server/FoodSquad_API/Models/DTO/User/LogoutReqestDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class LogoutRequestDTO
+{
+    [Required]
+    public string AccessToken { get; set; }
+
+    [Required]
+    public string RefreshToken { get; set; }
+}
---
+++ server/FoodSquad_API/Models/DTO/User/UserLoginDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FoodSquad_API.Models.DTO.User
+{
+    public class UserLoginDTO
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Email should be valid")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Size must be between 6 and 20 characters")]
+        public string Password { get; set; }
+    }
+}
