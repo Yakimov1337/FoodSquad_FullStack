ca477b5d01d613149e4b787d2cfaf4fd928d9f98
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-12-11 02:47:27 +0200

    Update MappingProfile.cs

--- server/FoodSquad_API/Mapper/MappingProfile.cs
+++ server/FoodSquad_API/Mapper/MappingProfile.cs
@@ -8,7 +8,6 @@ public class MappingProfile : Profile
 {
     public MappingProfile()
     {
-        // Order and OrderDTO
         CreateMap<Order, OrderDTO>()
          .ForMember(dest => dest.UserEmail, opt => opt.MapFrom(src => src.User.Email))
          .ForMember(dest => dest.MenuItemQuantities, opt => opt.MapFrom(src =>
