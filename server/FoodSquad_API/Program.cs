5526acbc11ded5510ca2c71de47447135f8119bb
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-12-11 14:03:48 +0200

    Allow deploy origin

--- server/FoodSquad_API/Program.cs
+++ server/FoodSquad_API/Program.cs
@@ -110,7 +110,7 @@ void ConfigureCors(IServiceCollection services)
         options.AddPolicy("CorsPolicy", policy =>
         {
             policy.WithOrigins("https://localhost:7238", "http://localhost:4200",
-                "https://food-squad-full-stack.vercel.app",) // Add allowed origin (Swagger,frontend and deployed version)
+                "https://food-squad-full-stack.vercel.app") // Add allowed origin (Swagger,frontend and deployed version)
                   .AllowAnyHeader() // Allow all headers
                   .AllowAnyMethod() // Allow all HTTP methods (GET, POST, etc.)
                   .AllowCredentials(); // Allow cookies and credentials
